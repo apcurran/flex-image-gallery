3397ae59485ba9e3eb4850623bb30b9da2eb51ac
apc <alexcurran.va@gmail.com>
2019-05-18 11:26:26 -0500

    Refactored code into an IIFE instead of using the global namespace

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const panels = document.querySelectorAll(".panel");
-
-panels.forEach((panel) => {
-    panel.addEventListener("click", animateOpen);
-});
-
-function animateOpen(event) {
-    const panel = event.target;
-    panel.classList.toggle("open");
-}
+const mainProgram = (() => {
+    const galleryContainer = document.querySelector(".gallery-container");
+    galleryContainer.addEventListener("click", animateOpen);
+ 
+    function animateOpen(event) {
+       const panel = event.target;
+       panel.classList.toggle("open");
+       console.log("Panel opened! IIFE worked!")
+    }
+  })();
